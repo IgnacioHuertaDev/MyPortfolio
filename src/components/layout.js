ab15a22378e32f269c62800a8abde521c5cd4bf9
NachoH <98nachohuerta@gmail.com>
2020-10-28 01:11:24 -0300

    Fix bug en el layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -58,8 +58,6 @@ const Layout = (props) => {
                 #___gatsby {
                     display: flex;
                     flex-direction: column;
-                    height: 100%;
-                    width: 100%;
                 }
 
                 #gatsby-focus-wrapper{
