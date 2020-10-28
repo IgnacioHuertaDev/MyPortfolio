4d693f2ec55ca41884dc0d5bc133e2a6cec1e2db
NachoH <98nachohuerta@gmail.com>
2020-10-28 15:12:52 -0300

    Fix pantalla 404

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Image from '../image'
-import { ContainerError } from './styles'
+import { ContainerError, ImageColumn } from './styles'
 import { Boton } from '../../styles/GlobalStyles'
 import { Link } from 'gatsby'
 
 export const Error404 = () => {
   return (
     <ContainerError>
-      <Image name='Error.png' />
-      <h1>An error has occurred</h1>
-      <Link to='/'>
-        <Boton>Back to home</Boton>
-      </Link>
+        <ImageColumn>
+            <Image name='Error.png' />
+        </ImageColumn>
+
+        <h1>An error has occurred</h1>
+        <Link to='/'>
+            <Boton>Back to home</Boton>
+        </Link>
     </ContainerError>
   )
 }
--- src/components/Error/styles.js
+++ src/components/Error/styles.js
@@ -16,3 +16,19 @@ export const ContainerError = styled.div`
   }
 
 `
+
+export const ImageColumn = styled.div`
+  width: 40%;
+  display: flex;
+  align-items: center;
+
+  & .gatsby-image-wrapper{
+    width: 80%;
+    margin: 30px auto 30px auto;
+  }
+
+  @media (max-width: 1025px) {
+    width: 100%;
+    justify-content: center;
+  }
+`
